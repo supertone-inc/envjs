afc6fb1474fd2f002d3db87c89fd9f26549fb88e
krazijames <krazijames@gmail.com>
2021-06-22 18:14:51 +0900

    Implement CLI

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-module.exports = function envjs(env) {
+#!/usr/bin/env node
+
+function setEnv(env) {
   for (const [key, value] of Object.entries(env)) {
     process.env[key] = value;
   }
-};
+}
+
+function main() {
+  const { execSync } = require("child_process");
+  const { existsSync } = require("fs");
+  const path = require("path");
+
+  const yargs = require("yargs/yargs");
+  const { hideBin } = require("yargs/helpers");
+
+  const pkg = require("./package.json");
+
+  const { argv } = yargs(hideBin(process.argv))
+    .usage("$0 [options] -- <command>")
+    .example(`$0 -- npm install ${pkg.name} --save-dev`)
+    .example(`$0 -e env.json -- npm install ${pkg.name} --save-dev`)
+    .option("env-file", {
+      alias: "e",
+      default: "env.js",
+      description: "A file to load (*.js|*.json)",
+    });
+
+  const envFilePath = path.resolve(process.cwd(), argv.envFile);
+
+  if (existsSync(envFilePath)) {
+    setEnv(require(envFilePath));
+  }
+
+  const command = argv._.join(" ");
+
+  if (!command) {
+    return;
+  }
+
+  execSync(command, { stdio: "inherit" });
+}
+
+if (require.main === module) {
+  main();
+} else {
+  module.exports = setEnv;
+}
--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
-const envjs = require(".");
+const setEnv = require(".");
 
-test("sets env variables", () => {
-  envjs(require("./env.sample.js"));
+test("function sets env variables", () => {
+  setEnv(require("./env.sample.js"));
 
   expect(process.env.STRING).toBe("1");
   expect(process.env.NUMBER).toBe("1");
@@ -11,4 +11,26 @@ test("sets env variables", () => {
   expect(process.env.UNDEFINED).toBe("undefined");
   expect(process.env.ARRAY).toBe("1,1,true,false,,");
   expect(process.env.OBJECT).toBe("[object Object]");
+});
+
+test("CLI sets env variables", () => {
+  const { execSync } = require("child_process");
+
+  const script = `\
+const assert = require("assert");\
+\
+assert.strictEqual(process.env.STRING, "1");\
+assert.strictEqual(process.env.STRING, "1");\
+assert.strictEqual(process.env.NUMBER, "1");\
+assert.strictEqual(process.env.BOOLEAN_TRUE, "true");\
+assert.strictEqual(process.env.BOOLEAN_FALSE, "false");\
+assert.strictEqual(process.env.NULL, "null");\
+assert.strictEqual(process.env.UNDEFINED, "undefined");\
+assert.strictEqual(process.env.ARRAY, "1,1,true,false,,");\
+assert.strictEqual(process.env.OBJECT, "[object Object]");\
+`;
+
+  execSync(
+    `./index.js -e env.sample.js -- node -e '${JSON.stringify(script)}'`
+  );
 });
