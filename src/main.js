4101988dadf915250fbee570c3dc95bc5d30fba6
krazijames <krazijames@gmail.com>
2021-06-26 14:28:16 +0900

    Fix errors

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 const { execSync } = require("child_process");
 const { resolve } = require("path");
 
-const parseArgs = require("./parseArgs");
+const { createArgParser } = require("./parseArgs");
 const parseEnvFile = require("./parseEnvFile");
 const replaceEnvVars = require("./replaceEnvVars");
 
 function main() {
-  const program = parseArgs();
+  const program = createArgParser();
 
-  if (!program.args.length) {
+  if (process.argv.length === 2) {
     program.help();
     return;
   }
+
+  program.parse();
 
   const envFilePath = resolve(program.opts().file);
   const env = { ...process.env, ...parseEnvFile(envFilePath) };
--- src/parseArgs.js
+++ src/parseArgs.js
@@ -3,23 +3,25 @@ const pkg = require("../package.json");
 
 const name = pkg.name.split("/")[1];
 
-module.exports = function parseArgs(argv, options) {
+function createArgParser() {
   return new Command()
     .name(name)
-    .arguments("<command>")
-    .description("", {
-      command: "a command to run with env variables",
-    })
+    .argument("<command>", "a command to run with env variables")
     .option("-f, --file <path>", "env file path", ".env.js")
     .helpOption("-h, --help", "display help")
     .addHelpText(
       "after",
       `
 Examples:
-  ${name} node -e "'console.log(process.env.ENV_VAR)'"
-  ${name} echo \\$ENV_VAR
+${name} node -e "'console.log(process.env.ENV_VAR)'"
+${name} echo \\$ENV_VAR
 `
     )
-    .passThroughOptions()
-    .parse(argv, options);
+    .passThroughOptions();
+}
+
+module.exports = function parseArgs(argv, options) {
+  return createArgParser().parse(argv, options);
 };
+
+module.exports.createArgParser = createArgParser;
